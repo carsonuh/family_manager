62843134739e7e3378a43836951ec1cc667d2197
Nabeel V <nabvali98@gmail.com>
2020-03-10 23:58:44 -0400

    Added base project

---
+++ family-manager/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Home from './components/Home.jsx';
+
+
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+export default App;
