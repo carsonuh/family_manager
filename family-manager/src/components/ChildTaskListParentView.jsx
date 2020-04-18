c891908ce2e60f352d3e47363a92972ae315c9e3
Carson Uecker-Herman <ueckerhc@mail.gvsu.edu>
2020-04-18 11:47:23 -0400

    More comments

--- family-manager/src/components/ChildTaskListParentView.jsx
+++ family-manager/src/components/ChildTaskListParentView.jsx
@@ -1,4 +1,4 @@
-import React from "react" 
+import React from "react"
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -7,24 +7,25 @@ import ClearIcon from '@material-ui/icons/Clear';
 import { red } from '@material-ui/core/colors';
 import Tooltip from '@material-ui/core/Tooltip';
 
+/**
+ * Displays all chores with a delete button for removing
+ * chores if necessary
+ * @param {chore, child, date} props 
+ */
 function ChildTaskListParentView(props) {
     let second = props.child + ", " + props.date
     return (
         <ListItem>
-            <ListItemText primary={props.chore} secondary={second}/>  
-
+            <ListItemText primary={props.chore} secondary={second} />
             <ListItemSecondaryAction>
-            <Tooltip title="Remove" aria-label="remove"> 
-                <IconButton edge="end" aria-label="delete" style={{ color: red[500] }} 
-                    onClick={() => props.onDeleteClick(props.chore, props.child, props.date)}>
-                      <ClearIcon />
-                </IconButton>
-            </Tooltip>
-
+                <Tooltip title="Remove" aria-label="remove">
+                    <IconButton edge="end" aria-label="delete" style={{ color: red[500] }}
+                        onClick={() => props.onDeleteClick(props.chore, props.child, props.date)}>
+                        <ClearIcon />
+                    </IconButton>
+                </Tooltip>
             </ListItemSecondaryAction>
-            
         </ListItem>
-        
     )
 }
 
