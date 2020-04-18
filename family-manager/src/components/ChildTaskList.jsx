7c0db2be58328e2a5336437223f5bb65a1781d15
Carson Uecker-Herman <ueckerhc@mail.gvsu.edu>
2020-04-18 11:45:14 -0400

    Comments for ChildTaskList complete

--- family-manager/src/components/ChildTaskList.jsx
+++ family-manager/src/components/ChildTaskList.jsx
@@ -1,4 +1,4 @@
-import React from "react" 
+import React from "react"
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -7,13 +7,15 @@ import DoneIcon from '@material-ui/icons/Done';
 import { green } from '@material-ui/core/colors';
 import Tooltip from '@material-ui/core/Tooltip';
 
+/**
+ * Displays childs chores in a list form with 
+ * button for marking complete 
+ * @param {chore, child, date} props 
+ */
 function ChildTaskList(props) {
-
     return (
         <ListItem>
-
-            <ListItemText primary={props.chore} secondary={props.date}/>
-
+            <ListItemText primary={props.chore} secondary={props.date} />
             <ListItemSecondaryAction>
                 <Tooltip title="Complete" aria-label="add">
                     <IconButton edge="end" aria-label="delete" style={{ color: green[500] }}
@@ -22,10 +24,7 @@ function ChildTaskList(props) {
                     </IconButton>
                 </Tooltip>
             </ListItemSecondaryAction>
-            
         </ListItem>
-        
     )
 }
-
 export default ChildTaskList
