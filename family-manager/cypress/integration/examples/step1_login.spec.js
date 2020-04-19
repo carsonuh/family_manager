ac827b4c44b56d7fa201ec83850e2ae09c890bba
Nabeel V <nabvali98@gmail.com>
2020-04-19 16:17:08 -0400

    The real final commit

--- family-manager/cypress/integration/examples/step1_login.spec.js
+++
@@ -1,6 +0,0 @@
-describe("Logging in", () => {
-    it('Logging In', () => {
-        cy.visit('localhost:3000')
-        cy.get('#loginButton').click()
-    });
-})
--- family-manager/cypress/integration/examples/step99_logout.spec.js
+++
@@ -1,9 +0,0 @@
-import { wait } from "@testing-library/react"
-
-
-describe("Logout", () => {
-    it("Click logout button", () => {
-        cy.wait(3000)
-        cy.get('.MuiToolbar-root > .MuiButton-root').click()
-    })
-})
